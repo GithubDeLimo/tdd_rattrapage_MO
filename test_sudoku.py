1b06887705dce1b63b57d6c976fc9663d388ff7b
GithubDeLimo <li._.mo@outlook.com>
2023-02-03 15:32:07 +0100

    test pour fonction colonne_valide()

--- test_sudoku.py
+++ test_sudoku.py
@@ -2,7 +2,7 @@ import sudoku
 import unittest
 
 
-class TestEx1(unittest.TestCase):
+class TestEx2(unittest.TestCase):
 
     def test_taille_valide(self):
         grille_taille_valide = [[1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -55,6 +55,30 @@ class TestEx1(unittest.TestCase):
 
         self.assertTrue(sudoku.ligne_valide(grille_ligne_valide))
         self.assertFalse(sudoku.ligne_valide(grille_ligne_non_valide))
+
+    def test_colonnne_valide(self):
+        grille_conlonne_valide = [[1, 2, 3, 4, 9, 6, 7, 8, 9],
+                                  [4, 6, 6, 7, 8, 9, 1, 2, 3],
+                                  [7, 8, 9, 1, 2, 3, 4, 3, 6],
+                                  [9, 1, 2, 3, 4, 1, 6, 7, 8],
+                                  [3, 4, 7, 6, 7, 8, 9, 1, 2],
+                                  [6, 7, 8, 9, 1, 2, 3, 4, 4],
+                                  [8, 9, 1, 2, 3, 4, 2, 6, 7],
+                                  [2, 3, 4, 8, 6, 7, 8, 9, 1],
+                                  [5, 5, 5, 5, 5, 5, 5, 5, 5]]
+
+        grille_conlonne_non_valide = [[1, 2, 3, 4, 5, 6, 1, 8, 9],
+                                      [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                                      [7, 8, 9, 1, 2, 3, 1, 5, 6],
+                                      [9, 1, 2, 3, 4, 5, 1, 7, 8],
+                                      [3, 4, 5, 6, 7, 8, 1, 1, 2],
+                                      [6, 7, 8, 9, 1, 2, 1, 4, 5],
+                                      [8, 9, 1, 2, 3, 4, 1, 6, 7],
+                                      [2, 3, 4, 5, 6, 7, 1, 9, 1],
+                                      [5, 6, 7, 8, 9, 1, 1, 3, 4]]
+
+        self.assertTrue(sudoku.colonne_valide(grille_conlonne_valide))
+        self.assertFalse(sudoku.colonne_valide(grille_conlonne_non_valide))
 
 
 if __name__ == '__main__':
